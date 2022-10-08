5e630ddc93ec421fb1481144ea77d22166c70db2
JimothyZ <ZajkowskJD13@uww.edu>
2022-10-08 15:27:34 -0500

    upgraded player camera
    LCtrl gives a top down view

--- Gorgon Stealth/Assets/Scripts/PlayerCam.cs
+++ Gorgon Stealth/Assets/Scripts/PlayerCam.cs
@@ -10,7 +10,9 @@ public class PlayerCam : MonoBehaviour
     public Transform orientation;
 
     float xRotation;
-    float yRotation; 
+    float yRotation;
+
+    private bool topDown = false;
 
     // Start is called before the first frame update
     private void Start()
@@ -22,15 +24,39 @@ public class PlayerCam : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {//get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            topDown = true;
+            GameObject.Find("CameraPos").transform.localPosition = new Vector3(0, 12, 0);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            topDown = false;
+            GameObject.Find("CameraPos").transform.localPosition = new Vector3(0, 0, 0);
+        }
 
-        //rotate cam
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        if(!topDown)
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+            yRotation += mouseX;
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            //rotate cam
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+        else
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            yRotation += mouseX;
+            transform.rotation = Quaternion.Euler(60, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+
+
     }
 }
